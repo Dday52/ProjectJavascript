f7474753a034a9a770478c707717324a7d8cf353
Akriosss <akriosss47@gmail.com>
2023-05-14 23:41:41 +0300

    проверка робота

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const a = 1;
-console.log(typeof(a))
+const res = prompt('Сколько будет 7 + или - 15?');
+switch (true) {
+    case res === 'Я не Робот':
+    case Number(res) === 22:
+    case Number(res) === - 8:
+        console.log('Успех');
+    break;
+    default:
+        console.log('Вы робот');
+}    
