e23fe2029513c7c19de9342efc07dffcaca270d6
Akriosss <akriosss47@gmail.com>
2023-05-22 00:17:40 +0300

    for of for in

---
+++ for of for in.js
@@ -0,0 +1,9 @@
+const arr = ['tiger', 'lion', 'elephant', 'dog', 'cat', 'monkey'];
+
+for (const element of arr) {
+    console.log(element);
+}
+
+for (const index in arr) {
+    console.log(index);
+}
