3ca90bf4a05cebb8e085841ee4e78e61450e6472
Akriosss <akriosss47@gmail.com>
2023-05-21 21:59:52 +0300

    Цикл

---
+++ Цикл в обратном порядке.js
@@ -0,0 +1,8 @@
+const arr = ['!', 'JS', 'love', 'I'];
+const resultArray = [];
+for(let i =  arr.length - 1; i>=0; i-- ){
+    resultArray.push(arr[i]);
+}
+console.log(resultArray.join(' '));
+
+console.log(arr.reverse().join(' '));
