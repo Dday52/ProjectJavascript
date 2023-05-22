6a24550e6e10efc2f698d541863d8a351b807411
Akriosss <akriosss47@gmail.com>
2023-05-22 22:11:31 +0300

    add function

--- forEach.js
+++ forEach.js
@@ -1,3 +1,9 @@
 const animals = ['cat', 'dog', 'mouse', 'lion'];
 animals.forEach(animal => {
     console.log(animal)});
+
+
+    const score = [5, 10, 0, 15];
+
+    score.forEach(element => {
+        console.log(`Round: ${element}`)})
